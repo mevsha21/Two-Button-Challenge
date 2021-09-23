cdd0e3c9862bc12275a37d69bff8b4c32225bc04
Olena Mevsha Adavi <omevshaadavi@zagmail.gonzaga.edu>
2021-09-22 21:12:41 -0700

    Completed Two-Button Pressed with textControl and text Alignment

--- Two-Button Challenge/ViewController.swift
+++ Two-Button Challenge/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLebel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func showMessagePressed(_ sender: UIButton) {
+        messageLebel.text = "You Are Awesome"
+        messageLebel.textColor = .blue
+        messageLebel.textAlignment = .left
+    }
+    @IBAction func showAnotherMessagePressed(_ sender: UIButton) {
+        messageLebel.text = "You Are Great"
+        messageLebel.textColor = .red
+        messageLebel.textAlignment = .right
+    }
+    
 }
 
